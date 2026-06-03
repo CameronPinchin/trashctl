2646e4ae2a931d0728e9d7d8b76fa8b2e4081b30
CameronPinchin <cameronpinchin@cmail.carleton.ca>
2026-06-02 22:27:21 -0400

    init.c changes

--- src/init.c
+++ src/init.c
@@ -154,11 +154,11 @@ static int trash_dir_create_dir_p_(struct environment_info* env)
     for(i = 0; i < 4; ++i) {
         fprintf(stderr, "c: %ld o: %ld\n", capacity, offset);
         remaining = capacity - offset;
-        if(err = snprintf(working_directory + offset, remaining, "/%s", parent_directories[i]) == -1){
+        if((err = snprintf(working_directory + offset, remaining, "/%s", parent_directories[i])) == -1){
             fprintf(stderr, "[ERROR]: %s\n", strerror(errno));
             return 1;
         }
-        fprintf(stderr, "[characters written] %d\n", written);
+        fprintf(stderr, "[characters written] %d\n", err);
         offset += err;
     }
     fprintf(stderr, "[success]: %s\n", working_directory);
