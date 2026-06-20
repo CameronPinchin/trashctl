f8ee4730533349aee496cc99d02d90a5b9b121c0
CameronPinchin <cameronpinchin@cmail.carleton.ca>
2026-06-20 10:37:14 -0400

    reorganize list operation to use a subshell

--- src/opt_list.c
+++ src/opt_list.c
@@ -1,33 +1,22 @@
 #include "../include/trashctl.h"
 /* This will cover the list command line option for trashctl */
 
-static int trashctl_list_operation(struct environment_info *env)
+static int init_shell(struct environment_info* env, char* shell_command)
 {
     int err, status;
     errno = 0;
 
-    pid_t pid_ls = fork();
+    pid_t pid_sh = fork();
 
-    if(pid_ls < 0){
-        fprintf(stderr, "[ERROR]: fork failure.\n");
+    if(pid_sh < 0){
         return 1;
-    } else if (pid_ls == 0){
-
-    char *t_usr_trash_dir = strdup(env->trash_dir);
-    char *t_usr_uname = strdup(env->uname);
-
-    char *const argv[] = {"ls", "-l", t_usr_trash_dir, NULL};
-    char *const envp[] = {t_usr_uname, t_usr_trash_dir, NULL};
-
-    execve(TRASHCTL_LS_PATH, argv, envp);
-
-    free(t_usr_uname);
-    free(t_usr_trash_dir);
-
+    } else if(pid_sh == 0){
+        /* child process */
+        if((err = execl(TRASHCTL_SH_PATH, "sh", "-c", shell_command, NULL)) == -1){
+            return 1;
+        }
     } else {
-
-        if((err = waitpid(pid_ls, &status, 0)) == -1){
-            fprintf(stderr, "[ERROR]: %s\n", strerror(errno));
+        if((err = waitpid(pid_sh, &status, 0)) == -1){
             return 1;
         }
 
@@ -36,6 +25,60 @@ static int trashctl_list_operation(struct environment_info *env)
 
     return 0;
 }
+
+static int trashctl_list_operation(struct environment_info *env)
+{
+    int err;
+    errno = 0;
+
+    char list_files_command[128] = "ls -l ";
+    char *cmd_ptr = list_files_command;
+
+    strcat(list_files_command, env->trash_dir);
+
+    if((err = init_shell(env, cmd_ptr) == 1)){
+        fprintf(stderr, "[ERROR] Fork failed for list operation.\n");
+        return 1;
+    }
+
+    return 0;
+}
+
+// static int trashctl_list_operation(struct environment_info *env)
+// {
+//     int err, status;
+//     errno = 0;
+//
+//     pid_t pid_ls = fork();
+//
+//     if(pid_ls < 0){
+//         fprintf(stderr, "[ERROR]: fork failure.\n");
+//         return 1;
+//     } else if (pid_ls == 0){
+//
+//     char *t_usr_trash_dir = strdup(env->trash_dir);
+//     char *t_usr_uname = strdup(env->uname);
+//
+//     char *const argv[] = {"ls", "-l", t_usr_trash_dir, NULL};
+//     char *const envp[] = {t_usr_uname, t_usr_trash_dir, NULL};
+//
+//     execve(TRASHCTL_LS_PATH, argv, envp);
+//
+//     free(t_usr_uname);
+//     free(t_usr_trash_dir);
+//
+//     } else {
+//
+//         if((err = waitpid(pid_ls, &status, 0)) == -1){
+//             fprintf(stderr, "[ERROR]: %s\n", strerror(errno));
+//             return 1;
+//         }
+//
+//         return 0;
+//     }
+//
+//     return 0;
+// }
 
 int trashctl_list(struct environment_info* env)
 {
